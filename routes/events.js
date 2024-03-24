52ab62cff953dfcef99584cb71f7098c6020c980
Haotian <Robertlht0828@gmail.com>
2024-03-24 14:31:27 -0400

    added

--- routes/events.js
+++ routes/events.js
@@ -5,6 +5,7 @@ import db from '../data/database.js';
 
 const router = Router();
 
+
 router.get('/', async (req, res) => {
   const allEvents = await db.collection('events').find().toArray();
   res.json({ events: allEvents });
